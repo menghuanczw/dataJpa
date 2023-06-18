aeccd19d49b6657c16a5a3a42960fb4c26acb580
ccc <menghuanczw@126.com>
2023-06-19 07:45:23 +0800

    修改tomcat 为 undertow

--- src/main/java/dataJpa/dao/RoleMenuDao.java
+++ src/main/java/dataJpa/dao/RoleMenuDao.java
@@ -1,12 +1,8 @@
 package dataJpa.dao;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.Repository;
-
 import dataJpa.bean.Role1;
+
 @org.springframework.stereotype.Repository
 public interface RoleMenuDao extends JpaRepository<Role1, Integer>{
 
--- src/main/java/dataJpa/dao/SystemRoleDao.java
+++ src/main/java/dataJpa/dao/SystemRoleDao.java
@@ -7,6 +7,8 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.Repository;
 
 import dataJpa.bean.SystemRole;
+
+
 @org.springframework.stereotype.Repository
 public interface SystemRoleDao extends Repository<SystemRole, Integer>{
 	@Query(value="from SystemRole where id = ?1")
